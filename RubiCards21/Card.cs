c57b7ea84c6e8000789a75d4ce7a366309806255
Lorenzo <lorenzo.coeugniet@hotmail.com>
2021-02-08 11:26:41 +0100

    COEUGNIET Lorenzo - RubiCards TP

---
+++ RubiCards21/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace CardBattle
+{
+    class Card : IComparable<Card>
+    {
+        public string Name { get; set; } = "";
+        public int Value { get; set; } = 0;
+
+        public Card(string name, int value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public int CompareTo([AllowNull] Card other)
+        {
+            return Value - other.Value;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- RubiCards21/Program.cs
+++ RubiCards21/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 
-namespace RubiCards21
+namespace CardBattle
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Deck deck = Deck.CreateDeck();
+
+            deck.Shuffle();
+
+            Console.WriteLine(deck);
+            Console.WriteLine("");
+
+            Card a = deck.GetTop();
+            Card b = deck.GetTop();
+            Card c = deck.GetLast();
+
+            Console.WriteLine(deck);
+            Console.WriteLine("");
+
+            if ( a.CompareTo(b) < 0 )
+            {
+                Console.WriteLine("A: " + a + "  < B: " + b);
+            } else if (a.CompareTo(b) > 0)
+            {
+                Console.WriteLine("A: " + a + "  > B: " + b);
+            } else
+            {
+                Console.Write("A: " + a + "  == B: " + b);
+            }
+
+            deck.Restock();
+
+            Console.WriteLine(deck);
+            Console.WriteLine("");
         }
     }
 }
