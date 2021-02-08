806fd0d6a99ab6c025cf06b65bdd139c5f94f73a
Robert Biaulet <r.biaulet@rubika-edu.com>
2021-02-08 11:27:36 +0100

    Initial commit

--- RubiCards21/Card.cs
+++ RubiCards21/Card.cs
@@ -2,66 +2,56 @@
 
 namespace Rubikards21
 {
-	public class Card : object, IComparable<Card>
-	{
-		public uint Value { get; private set; } = 0;
-		public uint Symbol { get; private set; } = 0;
+    public enum Value
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+    public enum Type
+    {
+        Clubs,
+        Hearts,
+        Diamonds,
+        Spades
+    }
+    
+    public struct Card : IComparable<Card>
+    {
+        public Card(Type type, Value value)
+        {
+            Type = type;
+            Value = value;
+        }
 
-		public string[] Symbols { get; private set; } = DefaultSymbols;
-		public string[] Values { get; private set; } = DefaultValues;
+        public Type Type { get; private set; }
+        public Value Value { get; private set; }
+        
+        public int CompareTo(Card other)
+        {
+            if (Type == other.Type) return Value.CompareTo(other.Value);
+            return Type.CompareTo(other.Type);
+        }
 
-		public readonly static string[] DefaultSymbols = new string[] { "♠", "♥", "♦", "♣" };
-		public readonly static string[] DefaultValues = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "R", "A" };
+        public static bool operator > (Card first, Card second) => first.CompareTo(second) > 0;
+        public static bool operator < (Card first, Card second) => first.CompareTo(second) < 0;
+        
+        public static bool operator >= (Card first, Card second) => first.CompareTo(second) >= 0;
+        public static bool operator <= (Card first, Card second) => first.CompareTo(second) <= 0;
+        
+        public static bool operator == (Card first, Card second) => first.CompareTo(second) == 0;
+        public static bool operator != (Card first, Card second) => first.CompareTo(second) != 0;
 
-		public Card(uint value, uint symbol)
-		{
-			Value = value;
-			Symbol = symbol;
-		}
-
-		public Card(uint value, uint symbol, string[] symbols, string[] values) : this(value, symbol)
-		{
-			Symbols = symbols;
-			Values = values;
-		}
-
-		public int CompareTo(Card other)
-		{
-			if (other == null) return 1;
-			if (Value == other.Value) return Symbol.CompareTo(other.Symbol);
-			return Value.CompareTo(other.Value);
-		}
-
-		public override string ToString()
-		{
-			return "|" + Symbols[Symbol] + Values[Value] + "|";
-		}
-
-		public static Card NewRandomCard() => NewRandomCard(DefaultSymbols, DefaultValues);
-
-		public static Card NewRandomCard(string[] symbols, string[] values)
-		{
-			var random = new Random();
-			uint randomValue = (uint)random.Next(values.Length - 1);
-			uint randomSymbol = (uint)random.Next(symbols.Length - 1);
-			return new Card(randomSymbol, randomValue, symbols, values);
-		}
-
-		public static Card[] NewDeck() => NewDeck(DefaultSymbols, DefaultValues);
-
-		public static Card[] NewDeck(string[] symbols, string[] values)
-		{
-			var symbolsLength = symbols.Length;
-			var valuesLength = values.Length;
-			var deck = new Card[symbolsLength * valuesLength];
-			for (uint s = 0; s < symbolsLength; s++)
-			{
-				for (uint v = 0; v < valuesLength; v++)
-				{
-					deck[s * valuesLength + v] = new Card(v, s, symbols, values);
-				}
-			}
-			return deck;
-		}
-	}
+        public override string ToString() => $"{Value} of {Type}";
+    }    
 }
--- RubiCards21/Program.cs
+++ RubiCards21/Program.cs
@@ -2,11 +2,135 @@
 
 namespace RubiCards21
 {
-    class Program
+class Program
     {
+        private static bool fastForward;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var deck = CreateDeck();
+
+            var halfLength = deck.Count / 2;
+            var firstDeck = new List<Card>(halfLength);
+            var secondDeck = new List<Card>(halfLength);
+
+            for (var i = 0; i < deck.Count; i++)
+            {
+                if (i % 2 == 0) firstDeck.Add(deck[i]);
+                else secondDeck.Add(deck[i]);
+            }
+            
+            ShowCommands();
+            Console.WriteLine("The match between Jérome & Tom begins");
+
+            while (firstDeck.Any() && secondDeck.Any())
+            {
+                var spoils = new Card[2]
+                {
+                    DrawFrom(firstDeck),
+                    DrawFrom(secondDeck)
+                };
+
+                if (spoils[0] == spoils[1]) throw new InvalidDataException("There should not be two of the same cards!");
+                else
+                {
+                    if (spoils[0] > spoils[1])
+                    {
+                        Console.WriteLine($"{spoils[0]} beats {spoils[1]}. Jérome wins the round");
+                        GiveSpoilsTo(spoils, firstDeck);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{spoils[1]} beats {spoils[0]}. Tom wins the round");
+                        GiveSpoilsTo(spoils, secondDeck);
+                    }
+                }
+                
+                Console.WriteLine($"Jérome has {firstDeck.Count} cards left & Tom has {secondDeck.Count} cards left.");
+                Console.WriteLine();
+                
+                if (!fastForward) ReadCommand();
+            }
+        }
+
+        private static List<Card> CreateDeck()
+        {
+            var types = Enum.GetValues(typeof(Type)).Cast<Type>();
+            var values = Enum.GetValues(typeof(Value)).Cast<Value>();
+
+            var length = types.Count() * values.Count();
+            
+            var deck = new List<Card>(length);
+            var indices = new List<int>(length);
+            for (var i = 0; i < length; i++)
+            {
+                deck.Add(default);
+                indices.Add(i);
+            }
+
+            var rnd = new Random();
+            foreach (var type in types)
+            {
+                foreach (var value in values)
+                {
+                    var i = rnd.Next(0, indices.Count);
+                    var index = indices[i];
+                    indices.RemoveAt(i);
+                    
+                    deck[index] = new Card(type, value);
+                }
+            }
+
+            return deck;
+        }
+
+        private static Card DrawFrom(IList<Card> deck)
+        {
+            var card = deck.First();
+            deck.RemoveAt(0);
+
+            return card;
+        }
+        private static void GiveSpoilsTo(IEnumerable<Card> spoils, IList<Card> deck)
+        {
+            foreach (var spoil in spoils) deck.Add(spoil);
+        }
+
+        private static void ReadCommand()
+        {
+            var state = true;
+            var line = Console.ReadLine();
+            
+            while (state)
+            {
+                switch (line)
+                {
+                    case "help":
+                        ShowCommands();
+                        line = Console.ReadLine();
+                        break;
+                    
+                    case "next":
+                        state = false;
+                        Console.WriteLine();
+                        break;
+                    
+                    case "skip":
+                        fastForward = true;
+                        state = false;
+                        Console.WriteLine();
+                        break;
+                }
+            }
+        }
+        
+        private static void ShowCommands()
+        {
+            Console.WriteLine("---| Commands :");
+            Console.WriteLine("------> help : shows commands");
+            Console.WriteLine("------> next : goes to next round");
+            Console.WriteLine("------> skip : fast forward through all rounds");
+            Console.WriteLine();
         }
     }
 }
