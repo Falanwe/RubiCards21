4f7aaa612c95fd5f40bc041de0cd38002080efdb
GOURDOL Jean-noël <jn.gourdol@rubika-edu.com>
2021-02-08 10:43:44 +0100

    Add project files.

---
+++ RubiCards21/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RubiCards21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
