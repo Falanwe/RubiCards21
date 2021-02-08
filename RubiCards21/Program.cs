c67ecdffbf88bd38d97f1bb41ac0ff171e71dc3f
Hugo R <@>
2021-02-08 11:33:40 +0100

    Card Class

--- RubiCards21/Program.cs
+++ RubiCards21/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
 
-namespace RubiCards21
+namespace Hura
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Card cardA = UserCreateCard();
+            Card cardB = UserCreateCard();
+            Console.WriteLine(GetGreatest(cardA, cardB).GetName());
+        }
+
+        public static Card GetGreatest(Card cardA, Card cardB)
+        {
+            return cardA > cardB ? cardA : cardB;
+        }
+
+        public static Card UserCreateCard()
+        {
+            Console.WriteLine("Enter card color : ");
+            string userInput = Console.ReadLine();
+            Color cardColor;
+            
+            while (!Enum.TryParse(userInput, true, out cardColor))
+            {
+                Console.WriteLine("Invalid color, accepted colors : ");
+                
+                foreach (Color color in Enum.GetValues(typeof(Color)))
+                {
+                    Console.WriteLine("- " + color);
+                }
+                    
+                Console.WriteLine("Enter color :");
+
+                userInput = Console.ReadLine();
+            }
+
+            Console.WriteLine("Enter card value : ");
+            userInput = Console.ReadLine();
+            Value cardValue;
+
+            while (!Enum.TryParse(userInput, true, out cardValue))
+            {
+                Console.WriteLine("Invalid value, accepted values : ");
+                
+                foreach (Value color in Enum.GetValues(typeof(Value)))
+                {
+                    Console.WriteLine("- " + color);
+                }
+                    
+                Console.WriteLine("Enter value :");
+
+                userInput = Console.ReadLine();
+            }
+            
+            Card card = new Card(cardColor, cardValue);
+
+            Console.WriteLine("[Created card : " + card.GetName() + "]");
+            
+            return card;
         }
     }
 }
