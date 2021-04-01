65a5265026f9d0ed67a304b9ea215cbed043b141
Jean Noël Gourdol <jngourdol@stormancer.com>
2021-04-01 15:30:53 +0200

    persistence in elasticsearch

---
+++ SampleWebService/Services/WeatherForecastDatabaseService.cs
@@ -0,0 +1,46 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebService.Services
+{
+    public class WeatherForecastDatabaseService : IWeatherForecastCacheService
+    {
+        public class CacheRecord
+        {
+            public string Id { get; set; } = "";
+            public List<WeatherForecast> List { get; set; } = new List<WeatherForecast>();
+        }
+
+        private readonly ElasticClient _client;
+
+        public WeatherForecastDatabaseService()
+        {
+            var connectionSettings = new ConnectionSettings(new Uri("http://localhost:9200"));
+            connectionSettings.DefaultIndex("weatherforecast");
+            _client = new ElasticClient(connectionSettings);
+        }
+
+        public async Task Empty()
+        {
+            await _client.DeleteAsync<CacheRecord>("cache");
+        }
+
+        public async Task<IEnumerable<WeatherForecast>> GetOrSet(Func<IEnumerable<WeatherForecast>> factory)
+        {
+            var response = await _client.GetAsync<CacheRecord>("cache");
+            if (response.Found)
+            {
+                return response.Source.List;
+            }
+            else
+            {
+                var value = factory().ToList();
+                var creationResponse = await _client.IndexDocumentAsync(new CacheRecord { List = value, Id ="cache" });
+                return value;
+            }
+        }
+    }
+}
--- SampleWebService/Startup.cs
+++ SampleWebService/Startup.cs
@@ -36,7 +36,7 @@ namespace SampleWebService
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
 
-            services.AddSingleton<IWeatherForecastCacheService, WeatherForecastCacheService>();
+            services.AddSingleton<IWeatherForecastCacheService, WeatherForecastDatabaseService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
