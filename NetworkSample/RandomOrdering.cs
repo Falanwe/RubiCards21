3eaab07d610c362440bc3ba59791b03b2a0ae6dc
Jean Noël Gourdol <jngourdol@stormancer.com>
2021-04-01 10:43:31 +0200

    Adds proper implementation of the Favorable detection algorithm

--- NetworkSample/RandomOrdering.cs
+++ NetworkSample/RandomOrdering.cs
@@ -16,25 +16,58 @@ namespace NetworkSample.Server
 
             var random = new Random();
 
-            for(byte i = 0; i<n; i++)
+            for (byte i = 0; i < n; i++)
             {
                 _order[i] = i;
             }
 
-            for(var i = (byte)(n-1); i>0; i--)
+            for (var i = (byte)(n - 1); i > 0; i--)
             {
-                var swapTarget = random.Next(i+1);
+                var swapTarget = random.Next(i + 1);
                 var temp = _order[i];
                 _order[i] = _order[swapTarget];
                 _order[swapTarget] = temp;
             }
         }
 
-        public bool IsFavorable => true;
-        
+        public bool IsFavorable => BiggestCycleLength() <= (_order.Length + 1) / 2;
+        private int BiggestCycleLength()
+        {
+            var result = 0;
+            for (var i = 0; i < _order.Length; i++)
+            {
+                var cycleLength = GetCycleLength(i);
+                if(cycleLength > result)
+                {
+                    result = cycleLength;
+                }
+            }
+            return result;
+        }
+
+        private int GetCycleLength(int start)
+        {
+            var result = 0;
+            var current = start;
+            do
+            {
+                result++;
+                current = _order[current];
+            }
+            while (current != start);
+            return result;
+        }
+
         public byte LookAt(byte index)
         {
-            return _order[index];
+            if (index < _order.Length)
+            {
+                return _order[index];
+            }
+            else
+            {
+                return 255;
+            }
         }
     }
 }
