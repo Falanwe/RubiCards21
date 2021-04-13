ae6983a5a3f91c550c5e841f4acc17cca08334dc
Hugo R <@>
2021-04-13 16:31:36 +0200

    Linked Swagger

--- Hangman/Startup.cs
+++ Hangman/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace Hangman
 {
@@ -26,6 +27,11 @@ namespace Hangman
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllers();
+
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo {Title = "My API", Version = "v1"});
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
