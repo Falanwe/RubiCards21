47988c7e98291ef2f1effd8ee1871f60d9f078cf
Jean Noël Gourdol <jngourdol@stormancer.com>
2021-04-15 16:49:30 +0200

    Adds an health check route

---
+++ ShiFuMiTournament/Controllers/HealthCheckController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShiFuMiTournament.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HealthCheckController : Controller
+    {
+
+        public IActionResult Get()
+        {
+            return Ok("it works!");
+        }
+    }
+}
