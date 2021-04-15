8e699930d86266a674e358aee62d038dfc40ddf2
Timothe <linkazer@gmail.com>
2021-04-15 11:12:50 +0200

    Correction du controller

--- ConnectFour/Controllers/ConnectFourController.cs
+++ ConnectFour/Controllers/ConnectFourController.cs
@@ -7,48 +7,45 @@ using ConnectFour.Models;
 
 namespace ConnectFour.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class ConnectFourController : ControllerBase
     {
-        [ApiController]
-        [Route("[controller]")]
-        public class ConnectFour : ControllerBase
+        private Dictionary<int, Game> currentGames;
+        private Game emptySlotGame;
+        private int lastGameId = 0;
+
+        [HttpGet("game")]
+        public async Task<IActionResult> GetGame()
         {
-            private Dictionary<int, Game> currentGames;
-            private Game emptySlotGame;
-            private int lastGameId = 0;
-
-            [HttpGet("game")]
-            public async Task<IActionResult> GetGame()
+            if (emptySlotGame == null)
             {
-                if(emptySlotGame==null)
-                {
-                    lastGameId++;
-                    emptySlotGame = new Game(lastGameId);
+                lastGameId++;
+                emptySlotGame = new Game(lastGameId);
 
-                    return Ok(await Task.FromResult(new GameForm() { GameId = emptySlotGame.gameId, PlayerId = 1 }));
-                }
-                else
-                {
-                    currentGames.Add(emptySlotGame.gameId, emptySlotGame);
-                    return Ok(await Task.FromResult(new GameForm() { GameId = emptySlotGame.gameId, PlayerId = 2 }));
-                }
-
-                //throw new NotImplementedException();
+                return Ok(await Task.FromResult(new GameForm() { GameId = emptySlotGame.gameId, PlayerId = 1 }));
+            }
+            else
+            {
+                currentGames.Add(emptySlotGame.gameId, emptySlotGame);
+                return Ok(await Task.FromResult(new GameForm() { GameId = emptySlotGame.gameId, PlayerId = 2 }));
             }
 
-            [HttpGet("{gameId}")]
-            public async Task<GameState> GetGameState(int gameId)
-            {
-                //Mettre la logique
-                throw new NotImplementedException();
-            }
+            //throw new NotImplementedException();
+        }
 
-            [HttpPost("{gameId}")]
-            public async Task<TileState[]> GetGameState(int gameId, [FromBody] IndividualPlay player)
-            {
-                //Mettre la logique
-                throw new NotImplementedException();
-            }
+        [HttpGet("{gameId}")]
+        public async Task<GameState> GetGameState(int gameId)
+        {
+            //Mettre la logique
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("{gameId}")]
+        public async Task<TileState[]> GetGameState(int gameId, [FromBody] IndividualPlay player)
+        {
+            //Mettre la logique
+            throw new NotImplementedException();
         }
     }
 }
