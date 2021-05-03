2d6b21b4e128cc488b704755bb469ff0a5bbb1c1
Jean Noël Gourdol <jngourdol@stormancer.com>
2021-05-03 14:30:33 +0200

    Adds HOST cont variable to easily change target server

--- TicTacToe.GreatClient/Services/TicTacToeService.cs
+++ TicTacToe.GreatClient/Services/TicTacToeService.cs
@@ -10,10 +10,12 @@ namespace TicTacToe.GreatClient.Services
 {
     class TicTacToeService
     {
+        private const string HOST = "http://10.51.2.23:666";
+
         public async Task<GameForm> FindGame()
         {
             var client = new HttpClient();
-            var response = await client.GetAsync("http://localhost:5000/TicTacToe/game");
+            var response = await client.GetAsync($"{HOST}/TicTacToe/game");
             response.EnsureSuccessStatusCode();
 
             var json = await response.Content.ReadAsStringAsync();
@@ -23,7 +25,7 @@ namespace TicTacToe.GreatClient.Services
         public async Task<TileState[]> TryGetBoard(string gameId)
         {
             var client = new HttpClient();
-            var response = await client.GetAsync($"http://localhost:5000/TicTacToe/{gameId}");
+            var response = await client.GetAsync($"{HOST}/TicTacToe/{gameId}");
 
             if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
             {
@@ -42,13 +44,13 @@ namespace TicTacToe.GreatClient.Services
 
             var play = new IndividualPlay()
             {
-                x = x,
-                y = y,
-                playerId = playerId
+                X = x,
+                Y = y,
+                PlayerId = playerId
             };
 
             var content = new StringContent(JsonConvert.SerializeObject(play), UTF8Encoding.UTF8, "application/json");
-            var response = await client.PostAsync($"http://localhost:5000/TicTacToe/{gameId}", content);
+            var response = await client.PostAsync($"{HOST}/TicTacToe/{gameId}", content);
 
             if (response.StatusCode == System.Net.HttpStatusCode.NotFound || response.StatusCode == System.Net.HttpStatusCode.BadRequest)
             {
@@ -64,7 +66,7 @@ namespace TicTacToe.GreatClient.Services
         public async Task<byte> GetResult(string gameId)
         {
             var client = new HttpClient();
-            var response = await client.GetAsync($"http://localhost:5000/TicTacToe/{gameId}/result");
+            var response = await client.GetAsync($"{HOST}/TicTacToe/{gameId}/result");
 
             if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
             {
